d589863c581345fa13b98ff22f21a488505470e1
dat <datdo@rocketmail.com>
2017-10-30 13:21:54 -0700

    Add paths

--- constants.py
+++ constants.py
@@ -1,7 +1,24 @@
 import torch
 
-use_cuda = torch.cuda.is_available()
-
+BATCH_SIZE = 128
+TEST_SIZE = 0.2
+TEACHER_FORCING_RATIO = 0.5
 MAX_LENGTH = 30
+LR = 0.001
+HIDDEN_SIZE = 512
+DIRECTIONS = 2
+LAYERS = 4
+DROPOUT = 0.05
+
 SOS_token = 0
 EOS_token = 1
+
+EN_PATH = "data/en.txt"
+JA_PATH = "data/ja.txt"
+EN_LANG_PATH = "data/en.pkl"
+JA_LANG_PATH = "data/ja.pkl"
+PAIRS_PATH = "data/en_ja_pairs.pkl"
+ENCODER_PATH = "data/encoder.model"
+DECODER_PATH = "data/decoder.model"
+
+use_cuda = torch.cuda.is_available()
