9034be7a4332fcec9e0f7ea5c3224a000c97fe39
dat <datdo@rocketmail.com>
2017-11-07 16:05:47 -0800

    Add remove_word to Lang class

--- Lang.py
+++ Lang.py
@@ -10,7 +10,7 @@ class Lang:
     def __init__(self, name):
         self.name = name
         self.word2index = {"SOS": 0, "EOS": 1}
-        self.word2count = {}
+        self.word2count = {"SOS": 0, "EOS": 0}
         self.index2word = {0: "SOS", 1: "EOS"}
         self.n_words = 2
 
@@ -26,6 +26,20 @@ class Lang:
             self.word2count[word] = 1
             self.index2word[self.n_words] = word
             self.n_words += 1
+
+    def remove_word(self, word):
+        if word != "SOS" and word != "EOS":
+            self.n_words -= 1
+            idx = self.word2index[word]
+            last_word = self.index2word[self.n_words]
+
+            del self.word2index[word]
+            del self.word2count[word]
+            del self.index2word[self.n_words]
+
+            if last_word != word:
+                self.word2index[last_word] = idx
+                self.index2word[idx] = last_word
 
 
 class LangDataset(Dataset):
